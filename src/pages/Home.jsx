5a89b713827bffda5133bbc2dd96aa56019d407d
Abheshek Murugan <abheshek.murugan@gmail.com>
2024-07-08 21:35:48 +0530

    favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,10 @@ import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import img from "../assets/home_image.png";
 import Sponsers from "../components/Sponsors";
 import About from "../components/About";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <Flex bgColor={"#9437AB"} flexDirection={"column"}>
       <Flex justifyContent={"space-between"} alignItems={"center"}>
@@ -14,7 +16,7 @@ const Home = () => {
           <Text maxW={"80%"} marginTop={"1%"}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt molestie dui at dictum. Cras congue, diam quis laoreet tincidunt, magna magna ullamcorper turpis,
           </Text>
-          <Button maxW={"40%"} marginTop={"3%"} bgColor={"#9A3BE4"} color={"white"} fontSize={"2xl"} fontWeight={100}>
+          <Button maxW={"40%"} marginTop={"3%"} bgColor={"#9A3BE4"} color={"white"} fontSize={"2xl"} fontWeight={100} onClick={() => navigate('/courses')}>
             Register
           </Button>
         </Flex>
